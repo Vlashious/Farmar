6a88f3f0015fbfa4e2b6dddffc298241798a6c38
Vlashious <flashro561@gmail.com>
2020-10-23 10:33:13 +0300

    new collision shape for plants. fixed plant spawn

--- EditModes/PlantMode.cs
+++ EditModes/PlantMode.cs
@@ -6,10 +6,15 @@ namespace Farmar
     {
         public static void Plant(TileMap ground, Vector2 pos, PackedScene plant)
         {
-            var newPlant = plant.Instance() as Plant;
-            var plantPos = ground.MapToWorld(ground.WorldToMap(pos));
-            newPlant.Position = plantPos;
-            ground.GetParent().AddChild(newPlant);
+            if (ground.GetCellv(ground.WorldToMap(pos)) != -1 && plant != null)
+            {
+                var newPlant = plant.Instance() as Plant;
+                var plantPos = ground.MapToWorld(ground.WorldToMap(pos));
+                plantPos.y += 4;
+                newPlant.Position = plantPos;
+                ground.GetParent().AddChild(newPlant);
+            }
+            else GD.Print("No ground beneath!");
         }
     }
 }
